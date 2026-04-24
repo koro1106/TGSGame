b1cdabaa829ce29b0c67f03f91a8a967c1ad2cf7
Okamoto <aaa>
2026-04-24 19:30:26 +0900

    一旦PlayerとEnemy統合

--- Assets/Okamoto/Main/script/Bullet/Bullet.cs
+++ Assets/Okamoto/Main/script/Bullet/Bullet.cs
@@ -3,9 +3,36 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float lifeTime = 3f;
+    private int damage = 20; // ダメージ量
+    private Vector2 direction;
+
 
     void Start()
     {
         Destroy(gameObject, lifeTime);
+    }
+   
+    // Playerから呼ばれる
+    public void SetDirection(Vector2 dir)
+    {
+        direction = dir;
+    }
+
+    
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            // Enemyスクリプト取得
+            Enemy enemy = other.GetComponent<Enemy>();
+
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage); // ダメージを与える
+            }
+
+            Destroy(gameObject); // 弾は消える
+        }
     }
 }
