08ee7404878e5c4522120246d034debd3297b2fe
Saijou <>
2026-04-26 13:06:41 +0900

    スキル取得でほかのノード表示可能に

--- Assets/Saijou/Scripts/SkillNodeUI.cs
+++ Assets/Saijou/Scripts/SkillNodeUI.cs
@@ -19,7 +19,8 @@ public class SkillNodeUI : MonoBehaviour
     public SkillData data;     // 紐づくスキルデータ
     public Image icon;         // ボタン画像
 
-    public SkillNodeUI[] nextNodes; // 次に解放されるスキル
+    public SkillNodeUI[] nextButtons; // 次に解放されるスキルボタン
+    public SkillLineUI[] nextLines;   // 次に解放されるスキルライン
 
     private SkillState state = SkillState.Locked; // 現在の状態（初期は未解放）
     public bool isStartNode = false; // 最初から表示するノード
@@ -31,8 +32,30 @@ public class SkillNodeUI : MonoBehaviour
             state = SkillState.Available;
         }
 
-        // 初期状態を反映
+
+        if (data.isUnlocked)
+        {
+            state = SkillState.Unlocked;
+        }
+        else if (isStartNode)
+        {
+            state = SkillState.Available;
+        }
+        else
+        {
+            state = SkillState.Locked;
+        }
+
         UpdateVisual();
+
+        // ラインも状態に応じて復元
+        foreach (var line in nextLines)
+        {
+            if (state == SkillState.Unlocked)
+                line.SetState(SkillState.Available);
+            else
+                line.SetState(SkillState.Locked);
+        }
     }
 
     // スキル解放
@@ -43,17 +66,23 @@ public class SkillNodeUI : MonoBehaviour
 
         // 状態を解放済みに変更
         state = SkillState.Unlocked;
+        data.isUnlocked = true;
+
         UpdateVisual();
 
         // 次のスキルを「解放可能状態」にする（透明度30）
-       foreach (var node in nextNodes)
+       foreach (var node in nextButtons) // ボタン
        {
-           node.SetAvailable();
+           node.SetButtonAvailable();
+       }
+       foreach (var line in nextLines) // ライン
+       {
+            line.SetState(SkillState.Available);
        }
    }
 
-    // スキルを「解放可能(うっすら表示)」状態にする
-    public void SetAvailable()
+    // スキルを「解放可能(うっすら表示)」状態にする(ボタン)
+    public void SetButtonAvailable()
     {
         // すでに解放済み or すでに表示中なら何もしない
         if (state != SkillState.Locked) return;
