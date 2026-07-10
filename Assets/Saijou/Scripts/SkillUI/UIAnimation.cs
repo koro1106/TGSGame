e1b891ae48f5dadc618016ed1b77ec30737e374a
sijou <q>
2026-07-10 18:00:38 +0900

    スキルツリーUI演出修正

--- Assets/Saijou/Scripts/SkillUI/UIAnimation.cs
+++ Assets/Saijou/Scripts/SkillUI/UIAnimation.cs
@@ -18,19 +18,21 @@ public class UIAnimation : MonoBehaviour
 
     IEnumerator Bounce(RectTransform rt)
     {
-        // 元のサイズ
-        Vector3 original = Vector3.one;
+        Vector3 original = Vector3.one;    // 元のサイズ
 
-        // 少し縮むサイズ
-        Vector3 small = original * 0.8f;
+        // 押した瞬間（横に少し広がって縦が潰れる）
+        Vector3 small = new Vector3(1.08f, 0.2f, 1f);
 
-        // 大きくなるサイズ
-        Vector3 big = original * 0.9f;
+        // 膨らむ（縦に少し伸びる）
+        Vector3 big = new Vector3(0.95f, 1.5f, 1f);
+
+        // 落ち着く
+        Vector3 settle = new Vector3(1.02f, 0.98f, 1f);
 
         // 回転（角度）
         Quaternion rot0 = Quaternion.identity;         // 元
-        Quaternion rot1 = Quaternion.Euler(0, 0, 8f); // 右に傾く
-        Quaternion rot2 = Quaternion.Euler(0, 0, -9f); // 左に傾く
+        Quaternion rot1 = Quaternion.Euler(0, 0, 5f); // 右に傾く
+        Quaternion rot2 = Quaternion.Euler(0, 0, -5f); // 左に傾く
 
         float t = 0;
 
@@ -38,7 +40,8 @@ public class UIAnimation : MonoBehaviour
         while(t < 0.05f)
         {
             t += Time.unscaledDeltaTime * speed; // timeScale無視(停止中でも動く)
-            float p = t / 0.05f;
+            //float p = t / 0.05f;
+            float p = Mathf.SmoothStep(0f, 1f, t / 0.05f);
 
             rt.localScale = Vector3.Lerp(original, small, p);
             rt.rotation = Quaternion.Lerp(rot0, rot1, p);
@@ -60,17 +63,33 @@ public class UIAnimation : MonoBehaviour
             yield return null;
         }
 
+        // ③少し縮む
         t = 0;
-
-        // ③元に戻る
-        while (t < 0.12f)
+        while (t < 0.08f)
         {
             t += Time.unscaledDeltaTime * speed;
-            float p = t / 0.12f;
+            float p = Mathf.SmoothStep(0, 1, t / 0.08f);
 
-            rt.localScale = Vector3.Lerp(big, original, p);
-            rt.rotation = Quaternion.Lerp(rot2, rot0, p);
+            rt.localScale = Vector3.Lerp(big, settle, p);
+            yield return null;
+        }
 
+        // ④元に戻る
+        t = 0;
+        while (t < 0.12f)
+        {
+            //t += Time.unscaledDeltaTime * speed;
+            //float p = t / 0.12f;
+
+            //rt.localScale = Vector3.Lerp(big, original, p);
+            //rt.rotation = Quaternion.Lerp(rot2, rot0, p);
+
+            //yield return null;
+
+            t += Time.unscaledDeltaTime * speed;
+            float p = Mathf.SmoothStep(0, 1, t / 0.08f);
+
+            rt.localScale = Vector3.Lerp(settle, original, p);
             yield return null;
         }
 
