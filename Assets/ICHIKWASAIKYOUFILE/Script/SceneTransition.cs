55346a03ca796658730e316a4a43074e103309b1
ichikawa <aaa>
2026-08-07 16:18:31 +0900

    タイトルでカーソルをボタンに合わせた時画像が変わるように、ゲームスタート時ロード画面追加

---
+++ Assets/ICHIKWASAIKYOUFILE/Script/SceneTransition.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    [SerializeField] private RectTransform fadeImage;
+
+    // 左端の位置
+    [SerializeField] private Vector2 endPos = Vector2.zero;
+
+    // 画面外(右)
+    [SerializeField] private Vector2 startPos = new Vector2(1920, 0);
+
+    [SerializeField] private float slideTime = 0.5f;
+
+    [SerializeField] private float waitTime = 0.5f;
+
+    public void StartGame()
+    {
+        StartCoroutine(Transition());
+    }
+
+    IEnumerator Transition()
+    {
+        fadeImage.anchoredPosition = startPos;
+
+        float t = 0;
+
+        while (t < slideTime)
+        {
+            t += Time.deltaTime;
+
+            fadeImage.anchoredPosition =
+                Vector2.Lerp(startPos, endPos, t / slideTime);
+
+            yield return null;
+        }
+
+        // 少し待つ
+        yield return new WaitForSeconds(waitTime);
+
+        SceneManager.LoadScene("MainStageScene");
+    }
+}
