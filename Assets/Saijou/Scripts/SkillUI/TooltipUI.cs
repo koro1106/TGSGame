63b57240323b005d8c8b09d8f9619341080cfc4b
Saijou <>
2026-08-19 22:31:19 +0900

    画像挿入・調整

--- Assets/Saijou/Scripts/SkillUI/TooltipUI.cs
+++ Assets/Saijou/Scripts/SkillUI/TooltipUI.cs
@@ -177,8 +177,8 @@ public class TooltipUI : MonoBehaviour
 
         // 固定オフセット（上下だけ切替）
         Vector2 offset = isUpper
-            ? new Vector2(-60, -400f)
-            : new Vector2(-60, 200f);
+            ? new Vector2(-60, -450f)
+            : new Vector2(-60, 300f);
 
         // 最終位置
         tooltipRect.position = screenPos + offset;
